0c6f0f92bdeef234c029c2f50d115e68b06dcb7e
Faisaleka21 <162295533+Faisaleka21@users.noreply.github.com>
2025-06-20 13:47:46 +0700

    Delete logout.php

--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_destroy(); // Destroy the session to log out the use
-?>
-<script type="text/javascript">
-    // Redirect to the login page after logout
-    location.href = 'loginUser.php';
-</script>
